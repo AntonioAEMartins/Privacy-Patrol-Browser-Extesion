7192dd77c6c60364cc67352d1ce9adcb66c7b507
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2024-05-13 12:03:52 -0300

    Delete: Removendo Arquivos de React

--- privacy-patrol-extension/src/components/Security.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Security = () => {
-  return <div>Security settings and recommendations...</div>;
-}
-
-export default Security;
--- privacy-patrol-extension/src/components/Storage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Storage = () => {
-  return <div>Storage options and information...</div>;
-}
-
-export default Storage;
